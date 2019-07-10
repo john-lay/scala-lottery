3b5dd3af4db518934a3a34c525fffbd0ac8b26f9
john-lay <john_lay@hotmail.co.uk>
2019-07-10 14:30:46 +0100

    Add more cases for result equals two

--- test/controllers/LineModelSpec.scala
+++ test/controllers/LineModelSpec.scala
@@ -7,8 +7,28 @@ class LineModelSpec extends WordSpec {
 
   "A Line" when {
     "the sum of all numbers equals two" should {
-      "have a result equal to two" in {
+      "have a result equal to two for 1, 1, 0" in {
         val fixture = Line(numbers = (1, 1, 0))
+        assertResult(Line.RESULT_EQUAL_TWO)(fixture.result)
+      }
+      "have a result equal to two for 1, 0, 1" in {
+        val fixture = Line(numbers = (1, 0, 1))
+        assertResult(Line.RESULT_EQUAL_TWO)(fixture.result)
+      }
+      "have a result equal to two for 0, 1, 1" in {
+        val fixture = Line(numbers = (0, 1, 1))
+        assertResult(Line.RESULT_EQUAL_TWO)(fixture.result)
+      }
+      "have a result equal to two for 2, 0, 0" in {
+        val fixture = Line(numbers = (2, 0, 0))
+        assertResult(Line.RESULT_EQUAL_TWO)(fixture.result)
+      }
+      "have a result equal to two for 0, 2, 0" in {
+        val fixture = Line(numbers = (0, 2, 0))
+        assertResult(Line.RESULT_EQUAL_TWO)(fixture.result)
+      }
+      "have a result equal to two for 0, 0, 2" in {
+        val fixture = Line(numbers = (0, 0, 2))
         assertResult(Line.RESULT_EQUAL_TWO)(fixture.result)
       }
     }
