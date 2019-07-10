3f102a6cf9df062ab3034df70de6229b4feb20ce
john-lay <john_lay@hotmail.co.uk>
2019-07-10 14:11:33 +0100

    Test happy path when all numbers are zero

--- test/controllers/LineModelSpec.scala
+++ test/controllers/LineModelSpec.scala
@@ -6,10 +6,31 @@ import org.scalatest.WordSpec
 class LineModelSpec extends WordSpec {
 
   "A Line" when {
-    "first number equals 1 and second number equals 1 and third number equals 0" should {
+    "the sum of all numbers equals two" should {
       "have a result equal to two" in {
         val fixture = Line(numbers = (1, 1, 0))
         assertResult(Line.RESULT_EQUAL_TWO)(fixture.result)
+      }
+    }
+
+    "all numbers are zero" should {
+      "have a result all same" in {
+        val fixture = Line(numbers = (0, 0, 0))
+        assertResult(Line.RESULT_ALL_SAME)(fixture.result)
+      }
+    }
+
+    "all numbers are one" should {
+      "have a result all same" in {
+        val fixture = Line(numbers = (1, 1, 1))
+        assertResult(Line.RESULT_ALL_SAME)(fixture.result)
+      }
+    }
+
+    "all numbers are two" should {
+      "have a result all same" in {
+        val fixture = Line(numbers = (2, 2, 2))
+        assertResult(Line.RESULT_ALL_SAME)(fixture.result)
       }
     }
   }
